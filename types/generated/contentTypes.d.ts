0001e5cf78067cead6d1c8510d8dfaebad87beff
Muaviyaimran <muaviyaimran1122@gmail.com>
2025-01-22 04:27:24 +0500

    Removed Inversed by

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -624,7 +624,6 @@ export interface ApiSupplierSupplier extends Struct.CollectionTypeSchema {
       Schema.Attribute.SetMinMaxLength<{
         minLength: 2;
       }>;
-    part: Schema.Attribute.Relation<'oneToOne', 'api::part.part'>;
     publishedAt: Schema.Attribute.DateTime;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
